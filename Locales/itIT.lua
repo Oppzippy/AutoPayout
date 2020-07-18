d3e8fab05a1befb2974a6122f138e59f2ff0c1d9
Kyle Scheuing <oppzippy@gmail.com>
2020-07-18 03:28:32 -0400

    Add files for all locales

---
+++ Locales/itIT.lua
@@ -0,0 +1,25 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("HuokanPayout", "itIT")
+if not L then return end
+
+-- L.addon_name = "Huokan Payout"
+
+-- L.payout_setup = "Payout Setup"
+-- L.payout = "Payout"
+-- L.payout_csv = "Payout CSV"
+
+-- L.subject = "Subject"
+-- L.unit = "Unit"
+-- L.start_payout = "Start Payout"
+-- L.not_assigned_gold_value = "%s is not assigned a gold value"
+-- L.next = "Next"
+
+-- L.start = "Start"
+-- L.pause = "Pause"
+-- L.done = "Done"
+-- L.payout_in_progress = "Payout in progress. Please keep the mailbox open."
+-- L.unsent_mail = "Unsent Mail"
+-- L.must_be_at_mailbox = "You must be at a mailbox to start the payout."
+-- L.can_not_assign_negative_gold = "You can not send negative gold to %s."
+
+-- L.default_subject = "Default Subject"
+-- L.default_unit = "Default Unit"
