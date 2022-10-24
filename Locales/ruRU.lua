8e0cd14392de6c383d57e6ddfdfba58aa7adf862
Kyle Scheuing <kyle@kylescheuing.com>
2022-10-24 01:15:49 -0400

    Use curseforge localization

--- Locales/ruRU.lua
+++ Locales/ruRU.lua
@@ -3,25 +3,4 @@ local addonName = ...
 local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "ruRU")
 if not L then return end
 
-L.addon_name = "Auto Payout"
-
-L.payout_setup = "Настройка выплат"
-L.payout = "Выплата"
-L.payout_csv = "Выплата CSV"
-
-L.subject = "Тема"
-L.unit = "Ед. изм"
-L.start_payout = "Начать выплату"
-L.not_assigned_gold_value = "%s не присваивается золотая стоимость"
-L.next = "следующий"
-
-L.start = "Начало"
-L.pause = "Пауза"
-L.done = "Готово"
-L.payout_in_progress = "Выплата в процессе. Пожалуйста, держите почтовый ящик открытым."
-L.unsent_mail = "Неотправленная почта"
-L.must_be_at_mailbox = "Вы должны быть возле почтового ящика, чтобы начать выплату."
-L.can_not_assign_negative_gold = "Вы не можете отправить отрицательное золото %s."
-
-L.default_subject = "Тема по умолчанию"
-L.default_unit = "Ед. измерения по умолчанию"
+--@localization(locale="ruRU", handle-unlocalized="comment")@
